49b96bbe822630ff1c4e0b7a197c8175374508fe
kimchi0521 <jj.lee@kingsway.school.nz>
2025-10-08 14:40:00 +1300

    Variables

---
+++ variables.py
@@ -0,0 +1,10 @@
+# Demonstrate how variables work in python
+
+# Author: JJ Lee
+# Date: 8-10-2025
+# version 1.0
+
+# Different types of variables
+# Store a string
+greeting = "hello world"
+print(greeting)
