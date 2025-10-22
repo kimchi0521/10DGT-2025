a593f157cf0ebb7752dab455f0364739bed889b4
kimchi0521 <jj.lee@kingsway.school.nz>
2025-10-22 15:09:45 +1300

    Error checking

---
+++ error_checking.py
@@ -0,0 +1,72 @@
+# Error checking
+# Author: JJ Lee
+# Date: 22-10-2025
+# Version 1
+
+# Code that tests whether a valid input is given (v1)
+'''done = False  # Boolean variable
+while not done:
+    num = int(input("Please enter your value: ")) # int() stores as a integer not a string
+    done = True
+
+print(f"The number that you entered is {num}")'''
+
+# Code that tests whether a valid input is given (v1.1)
+# Use the try and except method to catch errors
+'''done = False
+while not done:
+    try: # This method tries for a valid input
+        num = float(input("Please enter a integer: "))
+        done = True
+
+    except ValueError:
+        print("That is not a valid float number. \n")
+
+print(f"The number that you entered is {num}")'''
+
+# Code that tests whether a valid input is given (v1.2)
+# Create a function to call every time I ask the user for a number
+# A function is a chunk of code that does something
+# Functions can be used over and over again
+# To use a function (call it), write out its name with paranthesis at the end
+# To create a function, start with the word def(define)
+'''def test_int_num():
+    done = False
+    while not done:
+        try:
+            num = float(input("Please enter your value: "))
+            done = True
+
+        except ValueError:
+            print("That is not a valid float number. \n")
+
+    print(f"The number that you entered is {num}")
+
+# Main routine
+test_int_num() # This calls the function so that we can use it'''
+
+# Code that tests whether a valid input is given (v1.3)
+# Use the function parameters to make my code more pythonic
+def test_int(question): # 'question is a placeholder
+    done = False
+    while not done:
+        error = "That is not a valid number. \n"
+        print(question)
+        try:
+            num = int(input())
+            done = True
+
+        except ValueError:
+            print(error)
+
+    return(num) # Gives back the value of num so that I can use it outside of the function.
+
+# Main routine
+num1 = test_int("Please enter your first number: ")
+print(f"Your first number is {num1}. \n")
+
+num2 = test_int("Please enter your second number: ")
+print(f"Your second number is {num2}. \n")
+
+sum = num1 + num2
+print(f"Your numbers added together is {sum}")
